ead60b9827c62b62022cd4c889ecee2264aa920c
Thanh Phong <94186669+thanhphong26@users.noreply.github.com>
2024-07-13 09:27:00 +0700

    first commit

---
+++ src/main/java/com/pnt/demo/SecurityWithOauth2Application.java
@@ -0,0 +1,13 @@
+package com.pnt.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityWithOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityWithOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pnt/demo/SecurityWithOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnt.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityWithOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
